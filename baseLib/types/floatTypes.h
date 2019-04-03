b181839fb7094aa3c2e87b46142442f41573351d
Aleksey Mendelenko <mail@hex-profile.net>
2019-04-03 14:32:37 +0300

    Save

--- baseLib/types/floatTypes.h
+++ baseLib/types/floatTypes.h
@@ -4,25 +4,6 @@
 
 //================================================================
 //
-// float16
-//
-//================================================================
-
-#ifndef HEXLIB_FLOAT16
-#define HEXLIB_FLOAT16
-
-struct float16
-{
-    uint16_t data;
-};
-
-static_assert(sizeof(float16) == 2, "");
-static_assert(alignof(float16) == 2, "");
-
-#endif
-
-//================================================================
-//
 // float32
 // float64
 //
@@ -46,5 +27,24 @@ static_assert(alignof(float16) == 2, "");
     #error Check the platform and define the types
 
 #endif
+
+#endif
+
+//================================================================
+//
+// float16
+//
+//================================================================
+
+#ifndef HEXLIB_FLOAT16
+#define HEXLIB_FLOAT16
+
+struct float16
+{
+    uint16_t data;
+};
+
+static_assert(sizeof(float16) == 2, "");
+static_assert(alignof(float16) == 2, "");
 
 #endif
