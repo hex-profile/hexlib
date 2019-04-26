cd3fd08a06356835352d0addc6d9b94286b764d9
Aleksey Mendelenko <mail@hex-profile.net>
2019-04-26 22:19:11 +0300

    bsplinePrefilter

---
+++ imgLibGpu/bsplinePrefilter/bsplinePrefilterSettings.h
@@ -0,0 +1,9 @@
+#pragma once
+
+//================================================================
+//
+// BSPLINE_CUBIC_PREFILTER_RANGE_EXTENSION_FACTOR
+//
+//================================================================
+
+#define BSPLINE_CUBIC_PREFILTER_RANGE_EXTENSION_FACTOR 3.f
