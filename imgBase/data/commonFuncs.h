d48b9821e01ae16ce5c65dd1d86c735872740b60
Aleksey Mendelenko <mail@hex-profile.net>
2019-05-14 16:41:25 +0300

    equalSize, equalLayers, GPU_LAYERED_MATRIX_PASS

--- imgBase/data/commonFuncs.h
+++ imgBase/data/commonFuncs.h
@@ -4,19 +4,7 @@
 
 //================================================================
 //
-// equalSize
-//
-//================================================================
-
-template <typename Type>
-sysinline bool equalSize(const Type& A)
-{
-    return true;
-}
-
-//================================================================
-//
-// equalSize
+// GetSize
 //
 //================================================================
 
@@ -31,55 +19,26 @@ struct GetSize;
             {return body;} \
     };
 
-
-//----------------------------------------------------------------
-
-template <typename TypeA, typename TypeB>
-sysinline bool equalSize(const TypeA& A, const TypeB& B)
-{
-    return allv(GetSize<TypeA>::func(A) == GetSize<TypeB>::func(B));
-}
-
 //================================================================
 //
 // equalSize for N arguments
 //
 //================================================================
 
-template <typename T0, typename T1, typename T2>
-sysinline bool equalSize(const T0& v0, const T1& v1, const T2& v2)
-    {return equalSize(v0, v1) && equalSize(v0, v2);}
-
-template <typename T0, typename T1, typename T2, typename T3>
-sysinline bool equalSize(const T0& v0, const T1& v1, const T2& v2, const T3& v3)
-    {return equalSize(v0, v1) && equalSize(v0, v2) && equalSize(v0, v3);}
-
-template <typename T0, typename T1, typename T2, typename T3, typename T4>
-static sysinline bool equalSize(const T0& v0, const T1& v1, const T2& v2, const T3& v3, const T4& v4)
-    {return equalSize(v0, v1) && equalSize(v0, v2) && equalSize(v0, v3) && equalSize(v0, v4);}
-
-template <typename T0, typename T1, typename T2, typename T3, typename T4, typename T5>
-sysinline bool equalSize(const T0& v0, const T1& v1, const T2& v2, const T3& v3, const T4& v4, const T5& v5)
-    {return equalSize(v0, v1) && equalSize(v0, v2) && equalSize(v0, v3) && equalSize(v0, v4) && equalSize(v0, v5);}
-
-template <typename T0, typename T1, typename T2, typename T3, typename T4, typename T5, typename T6>
-sysinline bool equalSize(const T0& v0, const T1& v1, const T2& v2, const T3& v3, const T4& v4, const T5& v5, const T6& v6)
-    {return equalSize(v0, v1) && equalSize(v0, v2) && equalSize(v0, v3) && equalSize(v0, v4) && equalSize(v0, v5) && equalSize(v0, v6);}
-
-template <typename T0, typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7>
-sysinline bool equalSize(const T0& v0, const T1& v1, const T2& v2, const T3& v3, const T4& v4, const T5& v5, const T6& v6, const T7& v7)
-    {return equalSize(v0, v1) && equalSize(v0, v2) && equalSize(v0, v3) && equalSize(v0, v4) && equalSize(v0, v5) && equalSize(v0, v6) && equalSize(v0, v7);}
-
-//================================================================
-//
-// equalLayers
-//
-//================================================================
-
-template <typename TypeA, typename TypeB>
-sysinline bool equalLayers(const TypeA& A, const TypeB& B)
+template <typename T0>
+sysinline bool equalSize(const T0& v0)
 {
-    return getLayerCount(A) == getLayerCount(B);
+    return true;
+}
+
+template <typename T0, typename... Types>
+sysinline bool equalSize(const T0& v0, const Types&... values)
+{
+    auto size = GetSize<T0>::func(v0);
+
+    bool ok = true;
+    int tmp[] = {(ok &= allv(size == GetSize<Types>::func(values)), 0)...};
+    return ok;
 }
 
 //================================================================
@@ -88,26 +47,18 @@ sysinline bool equalLayers(const TypeA& A, const TypeB& B)
 //
 //================================================================
 
-template <typename T0, typename T1, typename T2>
-sysinline bool equalLayers(const T0& v0, const T1& v1, const T2& v2)
-    {return equalLayers(v0, v1) && equalLayers(v0, v2);}
+template <typename T0>
+sysinline bool equalLayers(const T0& v0)
+{
+    return true;
+}
 
-template <typename T0, typename T1, typename T2, typename T3>
-sysinline bool equalLayers(const T0& v0, const T1& v1, const T2& v2, const T3& v3)
-    {return equalLayers(v0, v1) && equalLayers(v0, v2) && equalLayers(v0, v3);}
+template <typename T0, typename... Types>
+sysinline bool equalLayers(const T0& v0, const Types&... values)
+{
+    auto layerCount = getLayerCount(v0);
 
-template <typename T0, typename T1, typename T2, typename T3, typename T4>
-static sysinline bool equalLayers(const T0& v0, const T1& v1, const T2& v2, const T3& v3, const T4& v4)
-    {return equalLayers(v0, v1) && equalLayers(v0, v2) && equalLayers(v0, v3) && equalLayers(v0, v4);}
-
-template <typename T0, typename T1, typename T2, typename T3, typename T4, typename T5>
-sysinline bool equalLayers(const T0& v0, const T1& v1, const T2& v2, const T3& v3, const T4& v4, const T5& v5)
-    {return equalLayers(v0, v1) && equalLayers(v0, v2) && equalLayers(v0, v3) && equalLayers(v0, v4) && equalLayers(v0, v5);}
-
-template <typename T0, typename T1, typename T2, typename T3, typename T4, typename T5, typename T6>
-sysinline bool equalLayers(const T0& v0, const T1& v1, const T2& v2, const T3& v3, const T4& v4, const T5& v5, const T6& v6)
-    {return equalLayers(v0, v1) && equalLayers(v0, v2) && equalLayers(v0, v3) && equalLayers(v0, v4) && equalLayers(v0, v5) && equalLayers(v0, v6);}
-
-template <typename T0, typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7>
-sysinline bool equalLayers(const T0& v0, const T1& v1, const T2& v2, const T3& v3, const T4& v4, const T5& v5, const T6& v6, const T7& v7)
-    {return equalLayers(v0, v1) && equalLayers(v0, v2) && equalLayers(v0, v3) && equalLayers(v0, v4) && equalLayers(v0, v5) && equalLayers(v0, v6) && equalLayers(v0, v7);}
+    bool ok = true;
+    int tmp[] = {(ok &= (layerCount == getLayerCount(values)), 0)...};
+    return ok;
+}
