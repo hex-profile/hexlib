c2a879f8c0cfa42e3efe9efed79e8f88b18bbaac
Aleksey Mendelenko <mail@hex-profile.net>
2019-07-10 20:58:02 +0300

    Remake vector types.

--- baseLib/types/vectorTypes.h
+++ baseLib/types/vectorTypes.h
@@ -1,14 +1,16 @@
 #pragma once
 
+#ifndef HEXLIB_VECTOR_BASE_TYPES
+#define HEXLIB_VECTOR_BASE_TYPES
+
 #include "types/intTypes.h"
 #include "types/floatTypes.h"
 
-#ifndef HEXLIB_HOST_VECTOR_BASE_TYPES
-#define HEXLIB_HOST_VECTOR_BASE_TYPES
-
 //================================================================
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+//----------------------------------------------------------------
 //
-// Vector types.
+// Define vector types.
 //
 // The header should define the following vector types: base_x2 and base_x4 
 // where the base is {bool, int8, uint8, int16, uint16, int32, uint32, float16, float32}.
@@ -16,17 +18,92 @@
 // For each type, the function make_[type] should be defined,
 // where [type] is the name of the vector type.
 //
+//----------------------------------------------------------------
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+//================================================================
+
+//================================================================
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+//----------------------------------------------------------------
+//
+// Define vector types.
+//
+// The header should define the following vector types: base_x2 and base_x4 
+// where the base is {bool, int8, uint8, int16, uint16, int32, uint32, float16, float32}.
+//
+// For each type, the function make_[type] should be defined,
+// where [type] is the name of the vector type.
+//
+//----------------------------------------------------------------
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 //================================================================
 
 //================================================================
 //
-// Vector types (2-component)
+// TMP_STRUCT_WITH_ALIGNMENT
 //
 //================================================================
 
-#define TMP_MACRO_X2(vector, scalar, byteAlignment) \
+#if defined(__CUDA_ARCH__)
+
+    #define TMP_STRUCT_WITH_ALIGNMENT(name, byteAlignment) \
+        __declspec(align(byteAlignment)) struct name
+
+#else
+
+    #define TMP_STRUCT_WITH_ALIGNMENT(name, byteAlignment) \
+        struct alignas(byteAlignment) name
+
+#endif
+
+//================================================================
+//
+// CUDA Types
+//
+//================================================================
+
+#if defined(__CUDA_ARCH__)
+
+#define TMP_MACRO(scalar, cudaScalar) \
     \
-    struct alignas(byteAlignment) vector \
+    using scalar##_x2 = cudaScalar##2; \
+    \
+    inline scalar##_x2 make_##scalar##_x2(scalar x, scalar y) \
+        {return make_##cudaScalar##2(x, y);} \
+    \
+    using scalar##_x4 = cudaScalar##4; \
+    \
+    inline scalar##_x4 make_##scalar##_x4(scalar x, scalar y, scalar z, scalar w) \
+        {return make_##cudaScalar##4(x, y, z, w);}
+
+////
+
+TMP_MACRO(int8, char)
+TMP_MACRO(uint8, uchar)
+
+TMP_MACRO(int16, short)
+TMP_MACRO(uint16, ushort)
+
+TMP_MACRO(int32, int)
+TMP_MACRO(uint32, uint)
+
+TMP_MACRO(float32, float)
+
+////
+
+#undef TMP_MACRO
+
+#endif
+
+//================================================================
+//
+// 2-component
+//
+//================================================================
+
+#define TMP_MACRO(vector, scalar, byteAlignment) \
+    \
+    TMP_STRUCT_WITH_ALIGNMENT(vector, byteAlignment) \
     { \
         scalar x; \
         scalar y; \
@@ -40,31 +117,36 @@
         return tmp; \
     }
 
-TMP_MACRO_X2(bool_x2, bool, 2)
+TMP_MACRO(bool_x2, bool, 2)
+TMP_MACRO(float16_x2, float16, 4)
 
-TMP_MACRO_X2(int8_x2, int8_t, 2)
-TMP_MACRO_X2(uint8_x2, uint8_t, 2)
+#if !defined(__CUDA_ARCH__)
 
-TMP_MACRO_X2(int16_x2, int16_t, 4)
-TMP_MACRO_X2(uint16_x2, uint16_t, 4)
+TMP_MACRO(int8_x2, int8_t, 2)
+TMP_MACRO(uint8_x2, uint8_t, 2)
 
-TMP_MACRO_X2(int32_x2, int32_t, 8)
-TMP_MACRO_X2(uint32_x2, uint32_t, 8)
+TMP_MACRO(int16_x2, int16_t, 4)
+TMP_MACRO(uint16_x2, uint16_t, 4)
 
-TMP_MACRO_X2(float16_x2, float16, 4)
-TMP_MACRO_X2(float32_x2, float32, 8)
+TMP_MACRO(int32_x2, int32_t, 8)
+TMP_MACRO(uint32_x2, uint32_t, 8)
+TMP_MACRO(float32_x2, float32, 8)
 
-#undef TMP_MACRO_X2
+#endif
+
+////
+
+#undef TMP_MACRO
 
 //================================================================
 //
-// Vector types (4-component)
+// 4-component
 //
 //================================================================
 
-#define TMP_MACRO_X4(vector, scalar, byteAlignment) \
+#define TMP_MACRO(vector, scalar, byteAlignment) \
     \
-    struct alignas(byteAlignment) vector \
+    TMP_STRUCT_WITH_ALIGNMENT(vector, byteAlignment) \
     { \
         scalar x; \
         scalar y; \
@@ -82,26 +164,43 @@ TMP_MACRO_X2(float32_x2, float32, 8)
         return tmp; \
     }
 
-TMP_MACRO_X4(bool_x4, bool, 4)
+TMP_MACRO(bool_x4, bool, 4)
+TMP_MACRO(float16_x4, float16, 8)
 
-TMP_MACRO_X4(int8_x4, int8_t, 4)
-TMP_MACRO_X4(uint8_x4, uint8_t, 4)
+#if !defined(__CUDA_ARCH__)
 
-TMP_MACRO_X4(int16_x4, int16_t, 8)
-TMP_MACRO_X4(uint16_x4, uint16_t, 8)
+TMP_MACRO(int8_x4, int8_t, 4)
+TMP_MACRO(uint8_x4, uint8_t, 4)
 
-TMP_MACRO_X4(int32_x4, int32_t, 16)
-TMP_MACRO_X4(uint32_x4, uint32_t, 16)
+TMP_MACRO(int16_x4, int16_t, 8)
+TMP_MACRO(uint16_x4, uint16_t, 8)
 
-TMP_MACRO_X4(float16_x4, float16, 8)
-TMP_MACRO_X4(float32_x4, float32, 16)
+TMP_MACRO(int32_x4, int32_t, 16)
+TMP_MACRO(uint32_x4, uint32_t, 16)
+TMP_MACRO(float32_x4, float32, 16)
 
-#undef TMP_MACRO_X4
+#endif
+
+////
+
+#undef TMP_MACRO
 
 //================================================================
 //
+// Undef
+//
+//================================================================
+
+#undef TMP_STRUCT_WITH_ALIGNMENT
+
+//================================================================
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+//----------------------------------------------------------------
+//
 // Check that vector types have identical size and alignment on all systems.
 //
+//----------------------------------------------------------------
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 //================================================================
 
 #define TMP_MACRO(Type, typeSize, typeAlignment) \
@@ -145,4 +244,4 @@ TMP_MACRO(float32_x4, 16, 16);
 
 //----------------------------------------------------------------
 
-#endif // HEXLIB_HOST_VECTOR_BASE_TYPES
+#endif // HEXLIB_VECTOR_BASE_TYPES
--- imgBase/vectorTypes/vectorBase.h
+++ imgBase/vectorTypes/vectorBase.h
@@ -1,8 +1,18 @@
 #pragma once
 
+#ifndef HEXLIB_VECTOR_BASE_TYPES
+#define HEXLIB_VECTOR_BASE_TYPES
+
+#include "compileTools/compileTools.h"
+#include "numbers/float/floatBase.h"
+#include "numbers/int/intBase.h"
+#include "vectorTypes/half/halfBase.h"
+
 //================================================================
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+//----------------------------------------------------------------
 //
-// Vector types.
+// Define vector types.
 //
 // The header should define the following vector types: base_x2 and base_x4 
 // where the base is {bool, int8, uint8, int16, uint16, int32, uint32, float16, float32}.
@@ -10,22 +20,165 @@
 // For each type, the function make_[type] should be defined,
 // where [type] is the name of the vector type.
 //
+//----------------------------------------------------------------
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+//================================================================
+
+//================================================================
+//
+// TMP_STRUCT_WITH_ALIGNMENT
+//
 //================================================================
 
 #if defined(__CUDA_ARCH__)
-    #include "vectorBaseCuda.inl"
+
+    #define TMP_STRUCT_WITH_ALIGNMENT(name, byteAlignment) \
+        __declspec(align(byteAlignment)) struct name
+
 #else
-    #ifndef HEXLIB_HOST_VECTOR_BASE_TYPES
-    #define HEXLIB_HOST_VECTOR_BASE_TYPES
-        #include "vectorBaseHost.inl"
-    #endif
+
+    #define TMP_STRUCT_WITH_ALIGNMENT(name, byteAlignment) \
+        struct alignas(byteAlignment) name
 
 #endif
 
 //================================================================
 //
+// CUDA Types
+//
+//================================================================
+
+#if defined(__CUDA_ARCH__)
+
+#define TMP_MACRO(scalar, cudaScalar) \
+    \
+    using scalar##_x2 = cudaScalar##2; \
+    \
+    sysinline scalar##_x2 make_##scalar##_x2(scalar x, scalar y) \
+        {return make_##cudaScalar##2(x, y);} \
+    \
+    using scalar##_x4 = cudaScalar##4; \
+    \
+    sysinline scalar##_x4 make_##scalar##_x4(scalar x, scalar y, scalar z, scalar w) \
+        {return make_##cudaScalar##4(x, y, z, w);}
+
+////
+
+TMP_MACRO(int8, char)
+TMP_MACRO(uint8, uchar)
+
+TMP_MACRO(int16, short)
+TMP_MACRO(uint16, ushort)
+
+TMP_MACRO(int32, int)
+TMP_MACRO(uint32, uint)
+
+TMP_MACRO(float32, float)
+
+////
+
+#undef TMP_MACRO
+
+#endif
+
+//================================================================
+//
+// 2-component
+//
+//================================================================
+
+#define TMP_MACRO(vector, scalar, byteAlignment) \
+    \
+    TMP_STRUCT_WITH_ALIGNMENT(vector, byteAlignment) \
+    { \
+        scalar x; \
+        scalar y; \
+    }; \
+    \
+    sysinline vector make_##vector(scalar x, scalar y) \
+    { \
+        vector tmp; \
+        tmp.x = x; \
+        tmp.y = y; \
+        return tmp; \
+    }
+
+TMP_MACRO(bool_x2, bool, 2)
+TMP_MACRO(float16_x2, float16, 4)
+
+#if !defined(__CUDA_ARCH__)
+
+TMP_MACRO(int8_x2, int8, 2)
+TMP_MACRO(uint8_x2, uint8, 2)
+
+TMP_MACRO(int16_x2, int16, 4)
+TMP_MACRO(uint16_x2, uint16, 4)
+
+TMP_MACRO(int32_x2, int32, 8)
+TMP_MACRO(uint32_x2, uint32, 8)
+TMP_MACRO(float32_x2, float32, 8)
+
+#endif
+
+////
+
+#undef TMP_MACRO
+
+//================================================================
+//
+// 4-component
+//
+//================================================================
+
+#define TMP_MACRO(vector, scalar, byteAlignment) \
+    \
+    TMP_STRUCT_WITH_ALIGNMENT(vector, byteAlignment) \
+    { \
+        scalar x; \
+        scalar y; \
+        scalar z; \
+        scalar w; \
+    }; \
+    \
+    sysinline vector make_##vector(scalar x, scalar y, scalar z, scalar w) \
+    { \
+        vector tmp; \
+        tmp.x = x; \
+        tmp.y = y; \
+        tmp.z = z; \
+        tmp.w = w; \
+        return tmp; \
+    }
+
+TMP_MACRO(bool_x4, bool, 4)
+TMP_MACRO(float16_x4, float16, 8)
+
+#if !defined(__CUDA_ARCH__)
+
+TMP_MACRO(int8_x4, int8, 4)
+TMP_MACRO(uint8_x4, uint8, 4)
+
+TMP_MACRO(int16_x4, int16, 8)
+TMP_MACRO(uint16_x4, uint16, 8)
+
+TMP_MACRO(int32_x4, int32, 16)
+TMP_MACRO(uint32_x4, uint32, 16)
+TMP_MACRO(float32_x4, float32, 16)
+
+#endif
+
+////
+
+#undef TMP_MACRO
+
+//================================================================
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+//----------------------------------------------------------------
+//
 // Check that vector types have identical size and alignment on all systems.
 //
+//----------------------------------------------------------------
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 //================================================================
 
 //
@@ -77,6 +230,20 @@ TMP_MACRO(float32_x4, 16, 16);
 //
 
 #undef TMP_MACRO
+
+//----------------------------------------------------------------
+
+#endif // #ifndef HEXLIB_VECTOR_BASE_TYPES
+
+//================================================================
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+//----------------------------------------------------------------
+//
+// Define FOREACH lists for vector types.
+//
+//----------------------------------------------------------------
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+//================================================================
 
 //================================================================
 //
