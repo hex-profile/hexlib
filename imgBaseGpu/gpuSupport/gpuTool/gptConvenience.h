faac803b2e854967fdb40bfc0d6cf637ddcfde70
Aleksey Mendelenko <mail@hex-profile.net>
2019-07-23 18:40:24 +0300

    naming

--- imgBaseGpu/gpuSupport/gpuTool/gptConvenience.h
+++ imgBaseGpu/gpuSupport/gpuTool/gptConvenience.h
@@ -2,20 +2,20 @@
 
 //================================================================
 //
-// GPUTOOL_INDEXED_MATRIX
+// GPUTOOL_INDEXED_NAME
 // GPUTOOL_INDEXED_SAMPLER
 //
-// Convenience macros
+// Convenience macros.
 //
 //================================================================
 
-#define GPUTOOL_INDEXED_MATRIX(count, Type, name) \
-    PREP_FOR(count, GPT_INDEXED_MATRIX_FUNC, (Type, name))
+#define GPUTOOL_INDEXED_NAME(count, Type, name) \
+    PREP_FOR(count, GPT_INDEXED_NAME_FUNC, (Type, name))
 
-#define GPT_INDEXED_MATRIX_FUNC(k, args) \
-    GPT_INDEXED_MATRIX_FUNC2(k, PREP_ARG2_0 args, PREP_ARG2_1 args)
+#define GPT_INDEXED_NAME_FUNC(k, args) \
+    GPT_INDEXED_NAME_FUNC2(k, PREP_ARG2_0 args, PREP_ARG2_1 args)
 
-#define GPT_INDEXED_MATRIX_FUNC2(k, Type, name) \
+#define GPT_INDEXED_NAME_FUNC2(k, Type, name) \
     ((Type, name##k))
 
 //----------------------------------------------------------------
