7252f4baf5e340ad169477fabf45d74b169093aa
Aleksey Mendelenko <mail@hex-profile.net>
2019-07-31 20:47:37 +0300

    Strs

---
+++ cppBase/charType/strUtils.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <cstring>
+
+//================================================================
+//
+// strEqual
+//
+//================================================================
+
+sysinline bool strEqual(const char* a, const char* b)
+    {return strcmp(a, b) == 0;}
+
+sysinline bool strEqual(const wchar_t* a, const wchar_t* b)
+    {return wcscmp(a, b) == 0;}
