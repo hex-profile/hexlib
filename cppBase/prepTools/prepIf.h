36c0e21e8a4af127b2e32bbe405d13302670dfb0
Aleksey Mendelenko <mail@hex-profile.net>
2019-08-29 16:40:43 +0300

    * PREP_FOR supports zero count; * PREP_ENUM rework, +ENUMERATE supporting zero count; * PREP_IF/PREP_SELECT added;

---
+++ cppBase/prepTools/prepIf.h
@@ -0,0 +1,299 @@
+#pragma once
+
+#include "prepTools/prepBase.h"
+
+//================================================================
+//
+// PREP_BOOL
+//
+// for i in range(0, 256+1):
+//    print('#define PREP_BOOL_{i} {result}'.format(i = i, result = (0 if i == 0 else 1)))
+//
+//================================================================
+
+#define PREP_BOOL(value) \
+    PREP_PASTE(PREP_BOOL_, value)
+
+//----------------------------------------------------------------
+
+#define PREP_BOOL_0 0
+#define PREP_BOOL_1 1
+#define PREP_BOOL_2 1
+#define PREP_BOOL_3 1
+#define PREP_BOOL_4 1
+#define PREP_BOOL_5 1
+#define PREP_BOOL_6 1
+#define PREP_BOOL_7 1
+#define PREP_BOOL_8 1
+#define PREP_BOOL_9 1
+#define PREP_BOOL_10 1
+#define PREP_BOOL_11 1
+#define PREP_BOOL_12 1
+#define PREP_BOOL_13 1
+#define PREP_BOOL_14 1
+#define PREP_BOOL_15 1
+#define PREP_BOOL_16 1
+#define PREP_BOOL_17 1
+#define PREP_BOOL_18 1
+#define PREP_BOOL_19 1
+#define PREP_BOOL_20 1
+#define PREP_BOOL_21 1
+#define PREP_BOOL_22 1
+#define PREP_BOOL_23 1
+#define PREP_BOOL_24 1
+#define PREP_BOOL_25 1
+#define PREP_BOOL_26 1
+#define PREP_BOOL_27 1
+#define PREP_BOOL_28 1
+#define PREP_BOOL_29 1
+#define PREP_BOOL_30 1
+#define PREP_BOOL_31 1
+#define PREP_BOOL_32 1
+#define PREP_BOOL_33 1
+#define PREP_BOOL_34 1
+#define PREP_BOOL_35 1
+#define PREP_BOOL_36 1
+#define PREP_BOOL_37 1
+#define PREP_BOOL_38 1
+#define PREP_BOOL_39 1
+#define PREP_BOOL_40 1
+#define PREP_BOOL_41 1
+#define PREP_BOOL_42 1
+#define PREP_BOOL_43 1
+#define PREP_BOOL_44 1
+#define PREP_BOOL_45 1
+#define PREP_BOOL_46 1
+#define PREP_BOOL_47 1
+#define PREP_BOOL_48 1
+#define PREP_BOOL_49 1
+#define PREP_BOOL_50 1
+#define PREP_BOOL_51 1
+#define PREP_BOOL_52 1
+#define PREP_BOOL_53 1
+#define PREP_BOOL_54 1
+#define PREP_BOOL_55 1
+#define PREP_BOOL_56 1
+#define PREP_BOOL_57 1
+#define PREP_BOOL_58 1
+#define PREP_BOOL_59 1
+#define PREP_BOOL_60 1
+#define PREP_BOOL_61 1
+#define PREP_BOOL_62 1
+#define PREP_BOOL_63 1
+#define PREP_BOOL_64 1
+#define PREP_BOOL_65 1
+#define PREP_BOOL_66 1
+#define PREP_BOOL_67 1
+#define PREP_BOOL_68 1
+#define PREP_BOOL_69 1
+#define PREP_BOOL_70 1
+#define PREP_BOOL_71 1
+#define PREP_BOOL_72 1
+#define PREP_BOOL_73 1
+#define PREP_BOOL_74 1
+#define PREP_BOOL_75 1
+#define PREP_BOOL_76 1
+#define PREP_BOOL_77 1
+#define PREP_BOOL_78 1
+#define PREP_BOOL_79 1
+#define PREP_BOOL_80 1
+#define PREP_BOOL_81 1
+#define PREP_BOOL_82 1
+#define PREP_BOOL_83 1
+#define PREP_BOOL_84 1
+#define PREP_BOOL_85 1
+#define PREP_BOOL_86 1
+#define PREP_BOOL_87 1
+#define PREP_BOOL_88 1
+#define PREP_BOOL_89 1
+#define PREP_BOOL_90 1
+#define PREP_BOOL_91 1
+#define PREP_BOOL_92 1
+#define PREP_BOOL_93 1
+#define PREP_BOOL_94 1
+#define PREP_BOOL_95 1
+#define PREP_BOOL_96 1
+#define PREP_BOOL_97 1
+#define PREP_BOOL_98 1
+#define PREP_BOOL_99 1
+#define PREP_BOOL_100 1
+#define PREP_BOOL_101 1
+#define PREP_BOOL_102 1
+#define PREP_BOOL_103 1
+#define PREP_BOOL_104 1
+#define PREP_BOOL_105 1
+#define PREP_BOOL_106 1
+#define PREP_BOOL_107 1
+#define PREP_BOOL_108 1
+#define PREP_BOOL_109 1
+#define PREP_BOOL_110 1
+#define PREP_BOOL_111 1
+#define PREP_BOOL_112 1
+#define PREP_BOOL_113 1
+#define PREP_BOOL_114 1
+#define PREP_BOOL_115 1
+#define PREP_BOOL_116 1
+#define PREP_BOOL_117 1
+#define PREP_BOOL_118 1
+#define PREP_BOOL_119 1
+#define PREP_BOOL_120 1
+#define PREP_BOOL_121 1
+#define PREP_BOOL_122 1
+#define PREP_BOOL_123 1
+#define PREP_BOOL_124 1
+#define PREP_BOOL_125 1
+#define PREP_BOOL_126 1
+#define PREP_BOOL_127 1
+#define PREP_BOOL_128 1
+#define PREP_BOOL_129 1
+#define PREP_BOOL_130 1
+#define PREP_BOOL_131 1
+#define PREP_BOOL_132 1
+#define PREP_BOOL_133 1
+#define PREP_BOOL_134 1
+#define PREP_BOOL_135 1
+#define PREP_BOOL_136 1
+#define PREP_BOOL_137 1
+#define PREP_BOOL_138 1
+#define PREP_BOOL_139 1
+#define PREP_BOOL_140 1
+#define PREP_BOOL_141 1
+#define PREP_BOOL_142 1
+#define PREP_BOOL_143 1
+#define PREP_BOOL_144 1
+#define PREP_BOOL_145 1
+#define PREP_BOOL_146 1
+#define PREP_BOOL_147 1
+#define PREP_BOOL_148 1
+#define PREP_BOOL_149 1
+#define PREP_BOOL_150 1
+#define PREP_BOOL_151 1
+#define PREP_BOOL_152 1
+#define PREP_BOOL_153 1
+#define PREP_BOOL_154 1
+#define PREP_BOOL_155 1
+#define PREP_BOOL_156 1
+#define PREP_BOOL_157 1
+#define PREP_BOOL_158 1
+#define PREP_BOOL_159 1
+#define PREP_BOOL_160 1
+#define PREP_BOOL_161 1
+#define PREP_BOOL_162 1
+#define PREP_BOOL_163 1
+#define PREP_BOOL_164 1
+#define PREP_BOOL_165 1
+#define PREP_BOOL_166 1
+#define PREP_BOOL_167 1
+#define PREP_BOOL_168 1
+#define PREP_BOOL_169 1
+#define PREP_BOOL_170 1
+#define PREP_BOOL_171 1
+#define PREP_BOOL_172 1
+#define PREP_BOOL_173 1
+#define PREP_BOOL_174 1
+#define PREP_BOOL_175 1
+#define PREP_BOOL_176 1
+#define PREP_BOOL_177 1
+#define PREP_BOOL_178 1
+#define PREP_BOOL_179 1
+#define PREP_BOOL_180 1
+#define PREP_BOOL_181 1
+#define PREP_BOOL_182 1
+#define PREP_BOOL_183 1
+#define PREP_BOOL_184 1
+#define PREP_BOOL_185 1
+#define PREP_BOOL_186 1
+#define PREP_BOOL_187 1
+#define PREP_BOOL_188 1
+#define PREP_BOOL_189 1
+#define PREP_BOOL_190 1
+#define PREP_BOOL_191 1
+#define PREP_BOOL_192 1
+#define PREP_BOOL_193 1
+#define PREP_BOOL_194 1
+#define PREP_BOOL_195 1
+#define PREP_BOOL_196 1
+#define PREP_BOOL_197 1
+#define PREP_BOOL_198 1
+#define PREP_BOOL_199 1
+#define PREP_BOOL_200 1
+#define PREP_BOOL_201 1
+#define PREP_BOOL_202 1
+#define PREP_BOOL_203 1
+#define PREP_BOOL_204 1
+#define PREP_BOOL_205 1
+#define PREP_BOOL_206 1
+#define PREP_BOOL_207 1
+#define PREP_BOOL_208 1
+#define PREP_BOOL_209 1
+#define PREP_BOOL_210 1
+#define PREP_BOOL_211 1
+#define PREP_BOOL_212 1
+#define PREP_BOOL_213 1
+#define PREP_BOOL_214 1
+#define PREP_BOOL_215 1
+#define PREP_BOOL_216 1
+#define PREP_BOOL_217 1
+#define PREP_BOOL_218 1
+#define PREP_BOOL_219 1
+#define PREP_BOOL_220 1
+#define PREP_BOOL_221 1
+#define PREP_BOOL_222 1
+#define PREP_BOOL_223 1
+#define PREP_BOOL_224 1
+#define PREP_BOOL_225 1
+#define PREP_BOOL_226 1
+#define PREP_BOOL_227 1
+#define PREP_BOOL_228 1
+#define PREP_BOOL_229 1
+#define PREP_BOOL_230 1
+#define PREP_BOOL_231 1
+#define PREP_BOOL_232 1
+#define PREP_BOOL_233 1
+#define PREP_BOOL_234 1
+#define PREP_BOOL_235 1
+#define PREP_BOOL_236 1
+#define PREP_BOOL_237 1
+#define PREP_BOOL_238 1
+#define PREP_BOOL_239 1
+#define PREP_BOOL_240 1
+#define PREP_BOOL_241 1
+#define PREP_BOOL_242 1
+#define PREP_BOOL_243 1
+#define PREP_BOOL_244 1
+#define PREP_BOOL_245 1
+#define PREP_BOOL_246 1
+#define PREP_BOOL_247 1
+#define PREP_BOOL_248 1
+#define PREP_BOOL_249 1
+#define PREP_BOOL_250 1
+#define PREP_BOOL_251 1
+#define PREP_BOOL_252 1
+#define PREP_BOOL_253 1
+#define PREP_BOOL_254 1
+#define PREP_BOOL_255 1
+#define PREP_BOOL_256 1
+
+//================================================================
+//
+// PREP_SELECT
+//
+//================================================================
+
+#define PREP_SELECT(cond, trueValue, falseValue) \
+    PREP_PASTE(PREP_SELECT_, PREP_BOOL(cond))(trueValue, falseValue)
+
+#define PREP_SELECT_0(trueValue, falseValue) falseValue
+#define PREP_SELECT_1(trueValue, falseValue) trueValue
+
+//================================================================
+//
+// PREP_IF
+//
+//================================================================
+
+#define PREP_IF(cond, trueValue) \
+    PREP_PASTE(PREP_IF_, PREP_BOOL(cond))(trueValue)
+
+#define PREP_IF_0(trueValue)
+#define PREP_IF_1(trueValue) trueValue
