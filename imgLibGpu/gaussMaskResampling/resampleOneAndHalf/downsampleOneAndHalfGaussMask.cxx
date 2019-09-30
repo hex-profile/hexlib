250f6d538ccbc7c42e5e25f87d6184b693a7f304
Aleksey Mendelenko <mail@hex-profile.net>
2019-09-30 14:21:12 +0300

    +types

--- imgLibGpu/gaussMaskResampling/resampleOneAndHalf/downsampleOneAndHalfGaussMask.cxx
+++ imgLibGpu/gaussMaskResampling/resampleOneAndHalf/downsampleOneAndHalfGaussMask.cxx
@@ -32,7 +32,8 @@ static const Space initialSrcShift = -2;
     \
     action(uint8, uint8, uint8, 1) \
     action(float16, float16, float16, 1) \
-    action(float32, float32, float32, 1)
+    action(float32, float32, float32, 1) \
+    action(float16_x4, float16_x4, float16_x4, 4)
 
 ////
 
