5d461ced8c415007dd6f27862b785fbd1eb69416
Aleksey Mendelenko <mail@hex-profile.net>
2019-11-20 15:00:17 +0300

    Better documentation and refactoring for the parallel reduction tools.

--- imgLibGpu/reductionTools.h
+++ imgLibGpu/reductionTools.h
@@ -1,46 +1,130 @@
 #pragma once
 
 //================================================================
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+//----------------------------------------------------------------
 //
-// PARALLEL_REDUCTION2
+// Generic parallel reduction algorithm, without memory storage aspects.
 //
+// On each iteration, each ith thread accumulates a value:
+//
+// if (i < activeCount) data[i] += data[stageSize + i]
+//
+// Usually ptr = &data[i] is precalculated inside the thread's state, 
+// so the operation looks like:
+//
+// if (i < activeCount) ptr[0] += ptr[stageSize]
+//
+// The reduction range is [0, reductionSize - 1]
+// The reduction thread index is named "reductionMember".
+//
+// The number of activeFlag threads:
+// stageSize + i < reductionSize <===> i < reductionSize - stageSize
+//
+//----------------------------------------------------------------
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 //================================================================
 
-#define PARALLEL_REDUCTION2_ITER(reductSize, reductMember, actionMacro, actionContext, stageSize) \
+#define PARALLEL_REDUCTION_TWICE(reductionSize, reductionMember, actionMacro, actionContext) \
+    \
+    COMPILE_ASSERT((reductionSize) <= 1024); \
+    PARALLEL_REDUCTION_TWICE_ITER(reductionSize, reductionMember, actionMacro, actionContext, 512) \
+    PARALLEL_REDUCTION_TWICE_ITER(reductionSize, reductionMember, actionMacro, actionContext, 256) \
+    PARALLEL_REDUCTION_TWICE_ITER(reductionSize, reductionMember, actionMacro, actionContext, 128) \
+    PARALLEL_REDUCTION_TWICE_ITER(reductionSize, reductionMember, actionMacro, actionContext, 64) \
+    PARALLEL_REDUCTION_TWICE_ITER(reductionSize, reductionMember, actionMacro, actionContext, 32) \
+    PARALLEL_REDUCTION_TWICE_ITER(reductionSize, reductionMember, actionMacro, actionContext, 16) \
+    PARALLEL_REDUCTION_TWICE_ITER(reductionSize, reductionMember, actionMacro, actionContext, 8) \
+    PARALLEL_REDUCTION_TWICE_ITER(reductionSize, reductionMember, actionMacro, actionContext, 4) \
+    PARALLEL_REDUCTION_TWICE_ITER(reductionSize, reductionMember, actionMacro, actionContext, 2) \
+    PARALLEL_REDUCTION_TWICE_ITER(reductionSize, reductionMember, actionMacro, actionContext, 1)
+
+//----------------------------------------------------------------
+
+#define PARALLEL_REDUCTION_TWICE_ITER(reductionSize, reductionMember, actionMacro, actionContext, stageSize) \
     { \
-        enum {_activeCount = COMPILE_CLAMP((reductSize) - stageSize, 0, stageSize)}; \
-        if (_activeCount) {bool active = (reductMember) < _activeCount; actionMacro(active, stageSize, actionContext)}; \
-        devSyncThreads(); \
+        constexpr Space _activeCount = COMPILE_CLAMP((reductionSize) - (stageSize), 0, stageSize); \
+        \
+        if (_activeCount != 0) /* Removes code for unused iterations at compile time. */ \
+        { \
+            bool _isActive = (reductionMember) < _activeCount; \
+            actionMacro(_isActive, stageSize, actionContext) \
+        } \
     }
 
-#define PARALLEL_REDUCTION2(reductSize, reductMember, actionMacro, actionContext) \
-    \
-    devSyncThreads(); \
-    \
-    COMPILE_ASSERT((reductSize) <= 256); \
-    PARALLEL_REDUCTION2_ITER(reductSize, reductMember, actionMacro, actionContext, 128) \
-    PARALLEL_REDUCTION2_ITER(reductSize, reductMember, actionMacro, actionContext, 64) \
-    PARALLEL_REDUCTION2_ITER(reductSize, reductMember, actionMacro, actionContext, 32) \
-    PARALLEL_REDUCTION2_ITER(reductSize, reductMember, actionMacro, actionContext, 16) \
-    PARALLEL_REDUCTION2_ITER(reductSize, reductMember, actionMacro, actionContext, 8) \
-    PARALLEL_REDUCTION2_ITER(reductSize, reductMember, actionMacro, actionContext, 4) \
-    PARALLEL_REDUCTION2_ITER(reductSize, reductMember, actionMacro, actionContext, 2) \
-    PARALLEL_REDUCTION2_ITER(reductSize, reductMember, actionMacro, actionContext, 1)
+//================================================================
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+//----------------------------------------------------------------
+//
+// Convenient tools to make reduction.
+//
+//----------------------------------------------------------------
+//
+// The tools support arbitrary number of parameters, 
+// each parameter is given by pair (Type, name).
+//
+// All parameters are passed as a preprocessor sequence (a) (b) (c), for example:
+// ((int, myInt)) ((float, myFloat).
+//
+//----------------------------------------------------------------
+//
+// The tools support N simultaneous parallel reductions:
+//
+// * outerSize is the number of reductions.
+// * outerMember is the current reduction index.
+//
+//----------------------------------------------------------------
+//
+// The usage example:
+//
+// REDUCTION_MAKE
+// (
+//     myName,
+//     tileSize, tileMember, // The number of reductions and the reduction index
+//     cellSize, cellMember, // The reduction size and the index inside the reduction
+//
+//     // List of pairs {Type, name}
+//     ((float32, sumWeight)) 
+//     ((float32_x2, sumWeightValue))
+//     ((float32_x2, sumWeightValueSq)),
+//
+//     {
+//         // Inside the body, each computation checks "active" variable.
+//         // (It checks "active" multiple times as it is efficient on GPU, predicated instructions)
+//         // L and R suffixes mean lvalue and rvalue.
+//
+//         if (active) *sumWeightL += *sumWeightR;
+//         if (active) *sumWeightValueL += *sumWeightValueR;
+//         if (active) *sumWeightValueSqL += *sumWeightValueSqR;
+//     }
+// )
+//
+//----------------------------------------------------------------
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+//================================================================
+
 
 //================================================================
 //
-// REDUCTION_SETUP
+// REDUCTION_PREPARE
+//
+// Allocates SRAM arrays for all enumerated parameters, 
+// each array for N=outerSize simultaneous reductions.
+//
+// Flat size and flat member are total ones for the flattened arrays.
+//
+// Each setup has a prefix and may be used multiple times.
 //
 //================================================================
 
-#define REDUCTION_SETUP(prefix, outerSize, outerMember, reductSize, reductMember, paramSeq) \
+#define REDUCTION_PREPARE(prefix, outerSize, outerMember, reductionSize, reductionMember, paramSeq) \
     \
-    const Space prefix##ReductSize = (reductSize); \
-    Space prefix##ReductMember = (reductMember); \
+    constexpr Space prefix##ReductionSize = (reductionSize); \
+    Space prefix##ReductionMember = (reductionMember); \
     \
-    const Space prefix##FlatSize = (outerSize) * prefix##ReductSize; \
-    Space prefix##ReductStart = (outerMember) * (reductSize); \
-    Space prefix##FlatMember = prefix##ReductStart + prefix##ReductMember; \
+    constexpr Space prefix##FlatSize = (outerSize) * prefix##ReductionSize; \
+    Space prefix##ReductionOrigin = (outerMember) * prefix##ReductionSize; \
+    Space prefix##FlatMember = prefix##ReductionOrigin + prefix##ReductionMember; \
     \
     REDUCTION_SETUPFLAT(prefix, prefix##FlatSize, prefix##FlatMember, paramSeq); \
 
@@ -57,8 +141,10 @@
     REDUCTION_SETUPFLAT_ITER4(PREP_ARG2_0 param, PREP_ARG2_1 param, prefix, flatSize, flatMember)
 
 #define REDUCTION_SETUPFLAT_ITER4(Type, name, prefix, flatSize, flatMember) \
-    devSramArray(PREP_PASTE3(prefix, name, ReductArray), Type, flatSize); \
-    ArrayPtr(Type) PREP_PASTE3(prefix, name, ReductPtr) = PREP_PASTE3(prefix, name, ReductArray) + flatMember;
+    \
+    devSramArray(PREP_PASTE3(prefix, name, ReductionArray), Type, flatSize); \
+    \
+    auto PREP_PASTE3(prefix, name, ReductionPtr) = PREP_PASTE3(prefix, name, ReductionArray) + (flatMember);
 
 //================================================================
 //
@@ -67,7 +153,10 @@
 //================================================================
 
 #define REDUCTION_STORE(prefix, paramSeq) \
-    PREP_FOR(PREP_SEQ_SIZE(paramSeq), REDUCTION_STOREFLAT_ITER, (prefix, paramSeq))
+    \
+    PREP_FOR(PREP_SEQ_SIZE(paramSeq), REDUCTION_STOREFLAT_ITER, (prefix, paramSeq)) \
+    \
+    devSyncThreads(); \
 
 #define REDUCTION_STOREFLAT_ITER(i, args) \
     REDUCTION_STOREFLAT_ITER2(i, PREP_ARG2_0 args, PREP_ARG2_1 args)
@@ -79,7 +168,7 @@
     REDUCTION_STOREFLAT_ITER4(PREP_ARG2_0 param, PREP_ARG2_1 param, prefix)
 
 #define REDUCTION_STOREFLAT_ITER4(Type, name, prefix) \
-    *PREP_PASTE3(prefix, name, ReductPtr) = name;
+    *PREP_PASTE3(prefix, name, ReductionPtr) = name;
 
 //================================================================
 //
@@ -100,50 +189,57 @@
     REDUCTION_READBACK_ITER4(PREP_ARG2_0 param, PREP_ARG2_1 param, prefix)
 
 #define REDUCTION_READBACK_ITER4(Type, name, prefix) \
-    name = PREP_PASTE3(prefix, name, ReductArray)[PREP_PASTE(prefix, ReductStart)];
+    name = PREP_PASTE3(prefix, name, ReductionArray)[PREP_PASTE(prefix, ReductionOrigin)];
 
 //================================================================
 //
-// REDUCTION_REDUCTION
+// REDUCTION_ACCUMULATE
 //
 //================================================================
 
-#define REDUCTION_ACCUM(prefix, paramSeq, accumBody) \
-    PARALLEL_REDUCTION2(prefix##ReductSize, prefix##ReductMember, REDUCTION_ACCUM_ITER, (prefix, paramSeq, accumBody))
+#define REDUCTION_ACCUMULATE(prefix, paramSeq, accumBody) \
+    PARALLEL_REDUCTION_TWICE(prefix##ReductionSize, prefix##ReductionMember, REDUCTION_ACCUM_ITER, (prefix, paramSeq, accumBody))
 
-#define REDUCTION_ACCUM_ITER(active, ofs, args) \
-    REDUCTION_ACCUM_ITER_EX(active, ofs, PREP_ARG3_0 args, PREP_ARG3_1 args, PREP_ARG3_2 args)
+#define REDUCTION_ACCUM_ITER(activeFlag, stageSize, args) \
+    REDUCTION_ACCUM_ITER_EX(activeFlag, stageSize, PREP_ARG3_0 args, PREP_ARG3_1 args, PREP_ARG3_2 args)
 
-#define REDUCTION_ACCUM_ITER_EX(active, ofs, prefix, paramSeq, accumBody) \
+#define REDUCTION_ACCUM_ITER_EX(activeFlag, stageSize, prefix, paramSeq, accumBody) \
     { \
-        PREP_FOR(PREP_SEQ_SIZE(paramSeq), REDUCTION_ACCUM_PREPARE, (prefix, paramSeq, active, ofs)) \
+        bool active = (activeFlag); \
+        PREP_FOR(PREP_SEQ_SIZE(paramSeq), REDUCTION_ACCUM_VARS, (prefix, paramSeq, activeFlag, stageSize)) \
         {accumBody;} \
+        devSyncThreads(); \
     }
 
-#define REDUCTION_ACCUM_PREPARE(i, args) \
-    REDUCTION_ACCUM_PREPARE1(i, PREP_ARG4_0 args, PREP_ARG4_1 args, PREP_ARG4_2 args, PREP_ARG4_3 args)
+//----------------------------------------------------------------
 
-#define REDUCTION_ACCUM_PREPARE1(i, prefix, paramSeq, active, ofs) \
-    REDUCTION_ACCUM_PREPARE2(PREP_SEQ_ELEM(i, paramSeq), prefix, active, ofs)
+#define REDUCTION_ACCUM_VARS(i, args) \
+    REDUCTION_ACCUM_VARS1(i, PREP_ARG4_0 args, PREP_ARG4_1 args, PREP_ARG4_2 args, PREP_ARG4_3 args)
 
-#define REDUCTION_ACCUM_PREPARE2(typeName, prefix, active, ofs) \
-    REDUCTION_ACCUM_PREPARE3(PREP_ARG2_0 typeName, PREP_ARG2_1 typeName, prefix, active, ofs)
+#define REDUCTION_ACCUM_VARS1(i, prefix, paramSeq, activeFlag, stageSize) \
+    REDUCTION_ACCUM_VARS2(PREP_SEQ_ELEM(i, paramSeq), prefix, activeFlag, stageSize)
 
-#define REDUCTION_ACCUM_PREPARE3(Type, name, prefix, active, ofs) \
-    ArrayPtr(Type) PREP_PASTE(name, L) = PREP_PASTE3(prefix, name, ReductPtr); \
-    ArrayPtr(const Type) PREP_PASTE(name, R) = PREP_PASTE3(prefix, name, ReductPtr) + ofs;
+#define REDUCTION_ACCUM_VARS2(typeName, prefix, activeFlag, stageSize) \
+    REDUCTION_ACCUM_VARS3(PREP_ARG2_0 typeName, PREP_ARG2_1 typeName, prefix, activeFlag, stageSize)
+
+#define REDUCTION_ACCUM_VARS3(Type, name, prefix, activeFlag, stageSize) \
+    ArrayPtr(Type) PREP_PASTE(name, L) = PREP_PASTE3(prefix, name, ReductionPtr); \
+    ArrayPtr(const Type) PREP_PASTE(name, R) = PREP_PASTE3(prefix, name, ReductionPtr) + (stageSize); /* make it const to avoid user mistakes */
 
 //================================================================
 //
-// REDUCTION_SIMPLE
+// REDUCTION_MAKE
 //
 //================================================================
 
-#define REDUCTION_SIMPLE_MAIN(prefix, paramSeq, accumBody) \
+#define REDUCTION_APPLY(prefix, paramSeq, accumBody) \
+    \
     REDUCTION_STORE(prefix, paramSeq) \
-    REDUCTION_ACCUM(prefix, paramSeq, accumBody) \
+    \
+    REDUCTION_ACCUMULATE(prefix, paramSeq, accumBody) \
+    \
     REDUCTION_READBACK(prefix, paramSeq)
 
-#define REDUCTION_SIMPLE(prefix, outerSize, outerMember, reductSize, reductMember, paramSeq, accumBody) \
-    REDUCTION_SETUP(prefix, outerSize, outerMember, reductSize, reductMember, paramSeq) \
-    REDUCTION_SIMPLE_MAIN(prefix, paramSeq, accumBody)
+#define REDUCTION_MAKE(prefix, outerSize, outerMember, reductionSize, reductionMember, paramSeq, accumBody) \
+    REDUCTION_PREPARE(prefix, outerSize, outerMember, reductionSize, reductionMember, paramSeq) \
+    REDUCTION_APPLY(prefix, paramSeq, accumBody)
