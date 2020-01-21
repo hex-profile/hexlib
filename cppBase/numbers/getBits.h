2f48099e7a180a2a6791ee23e7d5c9065212d697
Aleksey Mendelenko <mendelenko@arrival.com>
2020-01-21 14:54:55 +0300

    +getBits

---
+++ cppBase/numbers/getBits.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "numbers/mathIntrinsics.h"
+
+//================================================================
+//
+// getBits
+//
+//================================================================
+
+template <typename Float>
+sysinline Float getBits(Float err, int maxBits = 24)
+{
+    auto minValue = ldexp(Float(1), -maxBits);
+
+    if_not (def(err) && err >= minValue)
+        err = minValue;
+
+    return -nativeLog2(err);
+}
