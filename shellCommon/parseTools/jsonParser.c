60ab3e6687d7ca45bae095ed5081b1e67a1160c8
Aleksey Mendelenko <mendelenko@arrival.com>
2020-02-06 19:05:59 +0300

    +jsonParser

---
+++ shellCommon/parseTools/jsonParser.c
@@ -0,0 +1,256 @@
+#include "jsonParser.h"
+
+#include "charType/charArray.h"
+#include "compileTools/compileTools.h"
+#include "parseTools/parseTools.h"
+#include "storage/rememberCleanup.h"
+
+namespace jsonParser {
+
+//================================================================
+//
+// skipJsonValueBody
+//
+//================================================================
+
+template <typename Iterator>
+bool skipJsonValueBody(Iterator& ptr, Iterator end)
+{
+    auto s = ptr;
+
+    ////
+
+    ensure
+    (
+        skipCstr(s, end) ||
+        skipFloat(s, end) ||
+        skipText(s, end, STR("true")) ||
+        skipText(s, end, STR("false")) ||
+        skipText(s, end, STR("null"))
+    );
+
+    ptr = s;
+    return true;
+}
+
+//================================================================
+//
+// Kit
+//
+//================================================================
+
+template <typename Iterator>
+struct Kit
+{
+    Iterator end = nullptr;
+    Visitor<Iterator>* visitor = nullptr;
+};
+
+//----------------------------------------------------------------
+
+#define PARSE_BEG \
+    auto s = paramPtr; \
+    const auto end = kit.end
+
+#define PARSE_END \
+    paramPtr = s; \
+    return true
+
+//----------------------------------------------------------------
+
+#define ENSURE(condition, place) \
+    if (condition) ; else {kit.visitor->handleError(place); return false;}
+
+//================================================================
+//
+// parseElement
+//
+//================================================================
+
+template <typename Iterator>
+bool parseElement(Iterator& paramPtr, const Kit<Iterator>& kit);
+
+//================================================================
+//
+// parseMember
+//
+//================================================================
+
+template <typename Iterator>
+bool parseMember(Iterator& paramPtr, const Kit<Iterator>& kit)
+{
+    PARSE_BEG;
+
+    ////
+
+    skipAnySpace(s, end);
+
+    ////
+
+    auto keyStart = s;
+    ENSURE(skipCstr(s, end), s);
+
+    ENSURE(s >= keyStart + 2, keyStart);
+
+    Key<Iterator> key;
+    key.index = invalidIndex;
+    key.name = {keyStart + 1, s - 1};
+    ENSURE(kit.visitor->enter(key), keyStart);
+    REMEMBER_CLEANUP(kit.visitor->leave());
+
+    skipAnySpace(s, end);
+    ENSURE(skipTextThenAnySpace(s, end, STR(":")), s);
+
+    ////
+
+    ensure(parseElement(s, kit));
+
+    ////
+
+    PARSE_END;
+}
+
+//================================================================
+//
+// parseElement
+//
+//================================================================
+
+template <typename Iterator>
+bool parseElement(Iterator& paramPtr, const Kit<Iterator>& kit)
+{
+    PARSE_BEG;
+
+    ////
+
+    skipAnySpace(s, end);
+
+    ////
+
+    if (skipTextThenAnySpace(s, end, STR("{")))
+    {
+        if_not (skipTextThenAnySpace(s, end, STR("}")))
+        {
+            for (;;)
+            {
+                ensure(parseMember(s, kit));
+
+                if (skipTextThenAnySpace(s, end, STR("}")))
+                    break;
+
+                ENSURE(skipTextThenAnySpace(s, end, STR(",")), s);
+            }
+        }
+    }
+
+    ////
+
+    else if (skipTextThenAnySpace(s, end, STR("[")))
+    {
+        if_not (skipTextThenAnySpace(s, end, STR("]")))
+        {
+            for (Index currentIndex = 0; ; ++currentIndex)
+            {
+                Key<Iterator> key;
+                key.index = currentIndex;
+                key.name = {s, s};
+                ENSURE(kit.visitor->enter(key), s);
+                REMEMBER_CLEANUP(kit.visitor->leave());
+
+                ensure(parseElement(s, kit));
+
+                if (skipTextThenAnySpace(s, end, STR("]")))
+                    break;
+
+                ENSURE(skipTextThenAnySpace(s, end, STR(",")), s);
+            }
+        }
+    }
+
+    ////
+
+    else
+    {
+        auto valueStart = s;
+        ENSURE(skipJsonValueBody(s, end), valueStart);
+        kit.visitor->handleValue({valueStart, s});
+        skipAnySpace(s, end);
+    }
+
+    ////
+
+    skipAnySpace(s, end);
+
+    ////
+
+    PARSE_END;
+}
+
+//================================================================
+//
+// parseJson
+//
+//================================================================
+
+template <typename Iterator>
+bool parseJson(const Range<Iterator>& json, Visitor<Iterator>& visitor)
+{
+    Kit<Iterator> kit;
+    kit.end = json.end;
+    kit.visitor = &visitor;
+
+    auto ptr = json.ptr;
+    ensure(parseElement(ptr, kit));
+
+    ENSURE(ptr == json.end, ptr);
+    return true;
+}
+
+//================================================================
+//
+// getRowCol
+//
+//================================================================
+
+template <typename Iterator>
+void getRowCol(const Range<Iterator>& buffer, Iterator place, int& resX, int& resY)
+{
+    auto p = buffer.ptr;
+    auto end = buffer.end;
+
+    resX = 0; 
+    resY = 0;
+
+    for (int row = 0; ; ++row)
+    {
+        Iterator linePtr = 0;
+        Iterator lineEnd = 0;
+
+        if_not (getNextLine(p, end, linePtr, lineEnd))
+            break;
+
+        if (place >= linePtr && place < lineEnd)
+            {resX = place - linePtr; resY = row; break;}
+    }
+}
+
+//================================================================
+//
+// Instantiations.
+//
+//================================================================
+
+#define TMP_MACRO(Iterator) \
+    INSTANTIATE_FUNC_EX(parseJson<Iterator>, 0) \
+    INSTANTIATE_FUNC_EX(getRowCol<Iterator>, 1)
+
+TMP_MACRO(char*)
+TMP_MACRO(const char*)
+TMP_MACRO(wchar_t*)
+TMP_MACRO(const wchar_t*)
+
+#undef TMP_MACRO
+
+//----------------------------------------------------------------
+
+}
---
+++ shellCommon/parseTools/jsonParser.h
@@ -0,0 +1,218 @@
+#pragma once
+
+namespace jsonParser {
+
+//================================================================
+//
+// Range
+//
+//================================================================
+
+template <typename Iterator>
+struct Range
+{
+    Iterator ptr;
+    Iterator end;
+};
+
+//----------------------------------------------------------------
+
+template <typename Iterator>
+constexpr inline auto makeRange(Iterator ptr, Iterator end)
+    {return Range<Iterator>{ptr, end};}
+
+template <typename StaticArray>
+constexpr inline auto makeRangeFromArray(const StaticArray& staticArray)
+    {return makeRange(staticArray, staticArray + COMPILE_ARRAY_SIZE(staticArray));}
+
+//================================================================
+//
+// equal
+//
+//================================================================
+
+template <typename Iterator>
+inline bool operator ==(const Range<Iterator>& A, const Range<Iterator>& B);
+
+//================================================================
+//
+// Index
+//
+//================================================================
+
+using Index = unsigned;
+static constexpr Index invalidIndex = unsigned(-1);
+
+//================================================================
+//
+// Key
+//
+// Contains either a string or an index.
+// If it's index, the string is empty.
+// If it's string, the index is invalid index.
+//
+//================================================================
+
+template <typename Iterator>
+struct Key
+{
+    Range<Iterator> name;
+    Index index;
+};
+
+//----------------------------------------------------------------
+
+template <typename Iterator>
+constexpr inline auto makeKey(const Range<Iterator>& name)
+{
+    return Key<Iterator>{name, invalidIndex};
+}
+
+template <typename Literal>
+constexpr inline auto makeKey(const Literal& str)
+{
+    COMPILE_ASSERT(COMPILE_ARRAY_SIZE(str) >= 1);
+    return makeKey(makeRange(str, str + COMPILE_ARRAY_SIZE(str) - 1));
+}
+
+//----------------------------------------------------------------
+
+template <typename Iterator>
+inline bool operator ==(const Key<Iterator>& A, const Key<Iterator>& B)
+{
+    return 
+        A.index == B.index &&
+        A.name == B.name;
+}
+
+//================================================================
+//
+// equal
+//
+//================================================================
+
+template <typename Iterator>
+inline bool operator ==(const Range<Iterator>& A, const Range<Iterator>& B)
+{
+    auto sizeA = A.end - A.ptr; 
+    auto sizeB = B.end - B.ptr; 
+
+    ensure(sizeA == sizeB);
+
+    auto ptrA = A.ptr;
+    auto ptrB = B.ptr;
+
+    for (size_t i = 0; i != sizeA; ++i)
+    {
+        ensure(*ptrA == *ptrB);
+        ++ptrA; ++ptrB;
+    }
+
+    return true;
+}
+
+//================================================================
+//
+// Visitor
+//
+//================================================================
+
+template <typename Iterator>
+struct Visitor
+{               
+    // Enter namespace and leave namespace. 
+    // Enter function should control max depth and return false in case of error.
+    virtual bool enter(const Key<Iterator>& key) =0;
+    virtual void leave() =0;
+
+    // Handle value at current location. This function can interrupt processing
+    // by returning false, in this case parsing also returns false.
+    virtual bool handleValue(const Range<Iterator>& value) =0;
+
+    // Handle value at current location.
+    virtual void handleError(Iterator errorPlace) =0;
+};
+
+//================================================================
+//
+// PathVisitor
+//
+//================================================================
+
+template <typename Iterator>
+struct PathVisitor
+{
+    virtual bool handleValue(const Range<const Key<Iterator>*>& path, const Range<Iterator>& value) =0;
+    virtual void handleError(const Range<const Key<Iterator>*>& path, Iterator errorPlace) =0;
+};
+
+//================================================================
+//
+// PathVisitorImpl
+//
+//================================================================
+
+template <typename Iterator, int maxDepth = 64>
+class PathVisitorImpl : public Visitor<Iterator>
+{
+
+public:
+
+    PathVisitorImpl(PathVisitor<Iterator>& base)
+        : base(base) {}
+
+    virtual bool enter(const Key<Iterator>& key)
+    {
+        if_not (depth < maxDepth)
+            return false;
+
+        path[depth++] = key;
+        return true;
+    }
+
+    virtual void leave()
+    {
+        if (depth) 
+            --depth;
+    }
+
+    virtual bool handleValue(const Range<Iterator>& value)
+    {
+        return base.handleValue({path, path + depth}, value);
+    }
+
+    virtual void handleError(Iterator errorPlace)
+    {
+        return base.handleError({path, path + depth}, errorPlace);
+    }
+
+private:
+
+    Key<Iterator> path[maxDepth];
+    int depth = 0;
+
+    PathVisitor<Iterator>& base;
+
+};
+
+//================================================================
+//
+// parseJson
+//
+//================================================================
+
+template <typename Iterator>
+bool parseJson(const Range<Iterator>& json, Visitor<Iterator>& visitor);
+
+//================================================================
+//
+// getRowCol
+//
+//================================================================
+
+template <typename Iterator>
+void getRowCol(const Range<Iterator>& buffer, Iterator place, int& resX, int& resY);
+
+//----------------------------------------------------------------
+
+}
