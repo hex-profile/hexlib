b55cc2bb2de444766d3b7ff4d14f424747f75591
Aleksey Mendelenko <mendelenko@arrival.com>
2020-04-22 22:27:59 +0300

    parallelLoad.h -> use guarded pointers.

--- imgBaseGpu/gpuSupport/parallelLoad.h
+++ imgBaseGpu/gpuSupport/parallelLoad.h
@@ -21,53 +21,8 @@ sysinline void parallelLoadTypeNoSync(Space groupMember, const Type& srcParam, T
 
     ////
 
-    // ``` use debug pointers
-    const Atom* srcPtr = (const Atom*) &srcParam;
-    Atom* dstPtr = (Atom*) &dstParam;
-
-    ////
-
-    srcPtr += groupMember;
-    dstPtr += groupMember;
-
-    ////
-
-    PARALLEL_LOOP_UNBASED
-    (
-        i,
-        atomCount,
-        groupMember,
-        groupSize,
-        dstPtr[i] = LoadMode::func(&srcPtr[i])
-    );
-}
-
-//================================================================
-//
-// parallelLoadArrayNoSync
-//
-// Loads a structure from DRAM to SRAM by GPU group.
-//
-//================================================================
-
-template <Space groupSize, Space arraySize, typename LoadMode, typename Type>
-sysinline void parallelLoadArrayNoSync(Space groupMember, const Type* srcArray, Type* dstArray)
-{
-    using Atom = uint32;
-
-    ////
-
-    constexpr Space loadSize = sizeof(Type) * arraySize;
-
-    COMPILE_ASSERT(loadSize % sizeof(Atom) == 0);
-    constexpr Space atomCount = loadSize / sizeof(Atom);
-    COMPILE_ASSERT(alignof(Type) % alignof(Atom) == 0);
-
-    ////
-
-    // ``` use debug pointers
-    const Atom* srcPtr = (const Atom*) srcArray;
-    Atom* dstPtr = (Atom*) dstArray;
+    auto srcPtr = ArrayPtrCreate(const Atom, (const Atom*) &srcParam, atomCount, DbgptrArrayPreconditions());
+    auto dstPtr = ArrayPtrCreate(Atom, (Atom*) &dstParam, atomCount, DbgptrArrayPreconditions());
 
     ////
 
