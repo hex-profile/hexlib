83b780c3428833cfd696e52ca0d829b3e2a8fc17
Aleksey Mendelenko <mendelenko@arrival.com>
2020-06-01 22:32:48 +0300

    Added osErrors/errorLinux.

--- shellCommon/binaryFile/binaryFileImpl.h
+++ shellCommon/binaryFile/binaryFileImpl.h
@@ -11,6 +11,11 @@
     #include "binaryFileWin32.h"
     using BinaryFileImpl = BinaryFileWin32;
 
+#elif defined(__linux__)
+
+    #include "binaryFileLinux.h"
+    using BinaryFileImpl = BinaryFileLinux;
+
 #else
 
     #error
