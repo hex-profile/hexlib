1def68b7cb050cd945e5ea3e53feffa4e3081771
Aleksey Mendelenko <mail@hex-profile.net>
2021-04-20 18:19:26 +0300

    A bit of GPU atomics.

---
+++ imgBaseGpu/gpuDevice/gpuAtomics.h
@@ -0,0 +1,57 @@
+#pragma once
+
+#if !defined(__CUDA_ARCH__)
+    #include <atomic>
+#endif
+
+//================================================================
+//
+// atomicAdd
+//
+// Slow, but correct for x86.
+//
+//================================================================
+
+#if !defined(__CUDA_ARCH__)
+
+template <typename Type>
+inline void atomicAdd(Type* dst, Type value)
+{
+    using AtomicType = std::atomic<Type>;
+    COMPILE_ASSERT(sizeof(AtomicType) == sizeof(Type));
+    auto& target = * (AtomicType*) dst;
+
+    auto oldValue = target.load();
+
+    while (!target.compare_exchange_weak(oldValue, oldValue + value)) 
+        ;
+}
+
+#endif
+
+//================================================================
+//
+// atomicMax
+//
+// Slow, but correct for x86.
+//
+//================================================================
+
+#if !defined(__CUDA_ARCH__)
+
+template <typename Type>
+inline void atomicMax(Type* dst, Type value)
+{
+    using AtomicType = std::atomic<Type>;
+    COMPILE_ASSERT(sizeof(AtomicType) == sizeof(Type));
+    auto& target = * (AtomicType*) dst;
+
+    ////
+
+    auto oldValue = target.load();
+
+    while (!target.compare_exchange_weak(oldValue, maxv(oldValue, value))) 
+        ;
+}
+
+#endif
