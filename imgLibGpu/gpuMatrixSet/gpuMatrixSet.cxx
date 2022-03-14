bc1c72ab7a18e8edff6509e01e6e0ff6d6d1fb9d
Aleksey Mendelenko <mail@hex-profile.net>
2022-03-14 20:57:03 +0400

    More flexible gpuMatrixSet.

--- imgLibGpu/gpuMatrixSet/gpuMatrixSet.cxx
+++ imgLibGpu/gpuMatrixSet/gpuMatrixSet.cxx
@@ -1,54 +1,8 @@
-#include "gpuMatrixSet.h"
+#include "gpuMatrixSet.inl"
 
-#include "gpuSupport/gpuTool.h"
-#include "gpuSupport/gpuTexTools.h"
 #include "vectorTypes/vectorOperations.h"
-#include "gpuDevice/loadstore/storeNorm.h"
 
-//================================================================
-//
-// GPU_MATRIX_SET_FOREACH
-//
-//================================================================
+//----------------------------------------------------------------
 
-#define GPU_MATRIX_SET_FOREACH(action, extra) \
-    VECTOR_INT_FOREACH(action, extra) \
-    VECTOR_FLOAT_FOREACH(action, extra)
-
-//================================================================
-//
-// Functions
-//
-//================================================================
-
-#define TMP_MACRO(Type, o) \
-    \
-    GPUTOOL_2D \
-    ( \
-        gpuMatrixSetFunc_##Type, \
-        PREP_EMPTY, \
-        ((Type, dst)), \
-        ((Type, value)), \
-        Type v = value; \
-        *dst = v; \
-    )
-
-GPU_MATRIX_SET_FOREACH(TMP_MACRO, o)
-
-#undef TMP_MACRO
-
-//================================================================
-//
-// gpuMatrixSetFunc_*
-//
-//================================================================
-
-#define TMP_MACRO(Type, o) \
-    \
-    template <> \
-    stdbool gpuMatrixSet(const GpuMatrix<Type>& dst, const Type& value, stdPars(GpuProcessKit)) \
-        {return gpuMatrixSetFunc_##Type(dst, value, stdPassThru);}
-
-HOST_ONLY(GPU_MATRIX_SET_FOREACH(TMP_MACRO, o))
-
-#undef TMP_MACRO
+VECTOR_INT_FOREACH(GPU_MATRIX_SET_DEFINE, _) \
+VECTOR_FLOAT_FOREACH(GPU_MATRIX_SET_DEFINE, _)
---
+++ imgLibGpu/gpuMatrixSet/gpuMatrixSet.inl
@@ -0,0 +1,32 @@
+#include "gpuMatrixSet.h"
+
+#include "gpuSupport/gpuTool.h"
+#include "gpuSupport/gpuMixedCode.h"
+
+//================================================================
+//
+// GPU_MATRIX_SET_DEFINE
+//
+//================================================================
+
+#define GPU_MATRIX_SET_DEFINE(Type, o) \
+    \
+    GPUTOOL_2D \
+    ( \
+        gpuMatrixSetFunc_##Type, \
+        PREP_EMPTY, \
+        ((Type, dst)), \
+        ((Type, value)), \
+        Type v = value; \
+        *dst = v; \
+    ) \
+    \
+    HOST_ONLY(GPU_MATRIX_SET__HOST_FUNC(Type))
+
+//----------------------------------------------------------------
+
+#define GPU_MATRIX_SET__HOST_FUNC(Type) \
+    \
+    template <> \
+    stdbool gpuMatrixSet(const GpuMatrix<Type>& dst, const Type& value, stdPars(GpuProcessKit)) \
+        {return gpuMatrixSetFunc_##Type(dst, value, stdPassThru);}
